2cb408e81b1ca9d8540a824a92adb62f17ecadd5
Collin Smith <collink33@gmail.com>
2015-08-25 09:44:20 -0700

    Reorganize files

--- inheritance.js
+++
@@ -1,6 +0,0 @@
-Function.prototype.inherits = function (ParentClass) {
-  var Surrogate = function () {};
-  Surrogate.prototype = ParentClass.prototype;
-  this.prototype = new Surrogate();
-  this.prototype.constructor = this;
-};
